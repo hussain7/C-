3f3e89f4e38615b5748747d12ec30cc94da934fb
Muzahid Hussain <hussain7@users.noreply.github.com>
2017-04-23 12:24:58 +0200

    Create VisitorPattern.cpp

---
+++ VisitorPattern.cpp
@@ -0,0 +1,17 @@
+int my_visitor::operator()(int i) const
+{
+    return i;
+}
+ 
+int my_visitor::operator()(const std::string & str) const
+{
+    return str.length();
+}
+ 
+ 
+int answer()
+{
+    boost::variant< int, std::string > u("hello world");
+    int result = boost::apply_visitor( my_visitor(), u );
+    return result;
+}
